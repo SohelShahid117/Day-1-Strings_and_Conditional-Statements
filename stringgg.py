c4c8de86d1d0c0ffd850f283315e7c8f66bdeaaa
SohelShahid117 <sohelshahid09@gmail.com>
2024-06-17 23:58:16 +0600

    enjoy Python with string & conditional statement

---
+++ stringgg.py
@@ -0,0 +1,37 @@
+#https://youtu.be/lIId8IDP6TU?si=z-0AZQ6SlXPYPjob
+#String:
+str1 = "this is a string"
+str2 = 'js industry'
+str3 = """
+nJoy-->C
+nJoy-->C++
+nJoy-->Javascript
+nJoy-->python
+"""
+print(str1)
+print(str2)
+print(str3)
+print(str3[0])
+print(str1[0])
+print(str2[0])
+print(str2[1:])
+print(str2[1:4])
+print(str2[:4])
+print(str2[0:])
+print(str2[0:len(str2)])
+
+print("hello"+"world")
+print("hello"+" "+'world')
+print(len(str1))
+print(type(str1))
+
+fruit = "Apple" 
+print(fruit)
+print(fruit[1:])
+print(fruit[:3])
+print(fruit[-3:])
+print(fruit[:-1])
+print(fruit[-3:-1])
+print(fruit[-5:-2])
+
+# 20 min done
